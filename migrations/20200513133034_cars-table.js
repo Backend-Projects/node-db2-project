32abf677d9a43cafb0678c28aae7d6a79779cd68
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2020-05-13 13:49:44 -0400

    created 1st cars table migration

--- migrations/20200513133034_cars-table.js
+++ migrations/20200513133034_cars-table.js
@@ -1,8 +1,17 @@
-
-exports.up = function(knex) {
-  
+// "up" are the changes to be made to the db
+exports.up = function (knex) {
+  return knex.schema.createTable('cars', (tbl) => {
+    tbl.increments();
+    tbl.string('make').notNullable();
+    tbl.string('model').notNullable();
+    tbl.float('mileage').notNullable();
+    tbl.string('VIN', 17).unique().notNullable();
+    tbl.boolean('automatic').defaultTo(1);
+    tbl.string('title status', 20);
+  });
 };
 
-exports.down = function(knex) {
-  
+// "down" are the changes to be undone
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('cars');
 };
